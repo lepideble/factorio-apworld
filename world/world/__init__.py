894ef395c204eefb2194e0d7237767fe93a961b3
lepideble <147614625+lepideble@users.noreply.github.com>
2026-03-24 16:36:39 +0100

    Extract locations generation to a separate file

--- world/world/__init__.py
+++ world/world/__init__.py
@@ -5,7 +5,7 @@ from ..config import game_name
 from ..data import craftable_recipes, space_locations, surfaces, surfaces_accessible_at_start, technologies
 
 from .items import item_ids
-from .locations import location_ids, science_location_pools, FactorioLocation, FactorioScienceLocation
+from .locations import get_locations, location_ids
 from .options import FactorioOptions
 
 class FactorioItem(Item):
@@ -19,8 +19,6 @@ class FactorioWorld(World):
 
     options_dataclass = FactorioOptions
     options: FactorioOptions
-
-    science_locations: list[FactorioScienceLocation]
 
     def __init__(self, multiworld, player: int):
         super().__init__(multiworld, player)
@@ -48,42 +46,11 @@ class FactorioWorld(World):
 
             self.multiworld.regions.append(region)
 
-        # Create science locations
-        science_location_pool = []
+        for location in get_locations(self.options, self.random):
+            location.player = self.player
+            location.parent_region = menu_region
 
-        for pack in self.options.max_science_pack.get_allowed_packs():
-            science_location_pool.extend(science_location_pools[pack])
-
-        self.science_locations = []
-        for science_location_name in self.random.sample(science_location_pool, len(technologies)):
-            self.science_locations.append(FactorioScienceLocation(self.player, science_location_name, None, menu_region))
-
-        menu_region.locations.extend(self.science_locations)
-
-        # Attribute counts to science locations
-        cost_distribution = self.options.tech_cost_distribution
-        min_cost = self.options.min_tech_cost.value
-        max_cost = self.options.max_tech_cost.value
-
-        match cost_distribution:
-            case cost_distribution.option_even:
-                science_location_costs = (self.random.randint(min_cost, max_cost) for _ in self.science_locations)
-            case cost_distribution.option_low:
-                science_location_costs = (self.random.triangular(min_cost, max_cost, min_cost) for _ in self.science_locations)
-            case cost_distribution.option_middle:
-                science_location_costs = (self.random.triangular(min_cost, max_cost, (min_cost + max_cost) // 2) for _ in self.science_locations)
-            case cost_distribution.option_high:
-                science_location_costs = (self.random.triangular(min_cost, max_cost, max_cost) for _ in self.science_locations)
-
-        science_location_costs = sorted(science_location_costs)
-
-        if self.options.ramping_tech_costs:
-            sorter = lambda location: location.complexity, location.cost
-        else:
-            sorter = lambda location: location.cost
-
-        for i, location in enumerate(sorted(self.science_locations, key=sorter)):
-            location.count = science_location_costs[i]
+            menu_region.locations.append(location)
 
     def create_items(self) -> None:
         for technology in technologies:
--- world/world/locations.py
+++ world/world/locations.py
@@ -1,20 +1,23 @@
+from random import Random
+
 from BaseClasses import Location, Region
 
 from ..config import game_name
-from ..data import science_packs
+from ..data import technologies, science_packs
+from .options import FactorioOptions
 
 
-science_location_pools: dict[str, list[str]] = {}
+_science_location_pools: dict[str, list[str]] = {}
 
 for i, pack in enumerate(science_packs, start=1):
     max_needed: int = 999
     prefix: str = f"AP-{i}-"
-    science_location_pools[pack] = [prefix + str(x).upper().zfill(3) for x in range(1, max_needed + 1)]
+    _science_location_pools[pack] = [prefix + str(x).upper().zfill(3) for x in range(1, max_needed + 1)]
 
 location_ids = {}
 
 next_id = 1
-for pool in science_location_pools.values():
+for pool in _science_location_pools.values():
     location_ids.update({name: id for id, name in enumerate(pool, start=next_id)})
     next_id += len(pool)
 del next_id
@@ -30,15 +33,55 @@ class FactorioScienceLocation(FactorioLocation):
     ingredients: dict[str, int]
     count: int = 0
 
-    def __init__(self, player: int, name: str, address: int, parent: Region):
+    def __init__(self, player: int, name: str, address: int | None = None, parent: Region | None = None):
         super(FactorioScienceLocation, self).__init__(player, name, address, parent)
 
         # "AP-{Complexity}-{Cost}"
         self.complexity = int(self.name.split('-')[1]) - 1
         self.cost = int(self.name.split('-')[2])
 
-        self.ingredients = {science_packs[self.complexity]: 1}
-        for complexity in range(self.complexity):
-            if (parent.multiworld.worlds[self.player].options.tech_cost_mix >
-                    parent.multiworld.worlds[self.player].random.randint(0, 99)):
-                self.ingredients[science_packs[complexity]] = 1
+
+def get_locations(options: FactorioOptions, random: Random) -> list[FactorioLocation]:
+    # Create science locations
+    science_location_pool = []
+
+    for pack in options.max_science_pack.get_allowed_packs():
+        science_location_pool.extend(_science_location_pools[pack])
+
+    science_locations = []
+    for science_location_name in random.sample(science_location_pool, len(technologies)):
+        science_locations.append(FactorioScienceLocation(0, science_location_name))
+
+    # Attribute ingredients to science locations
+    for science_location in science_locations:
+        science_location.ingredients = {science_packs[science_location.complexity]: 1}
+        for complexity in range(science_location.complexity):
+            if (options.tech_cost_mix > random.randint(0, 99)):
+                science_location.ingredients[science_packs[science_location.complexity]] = 1
+
+    # Attribute counts to science locations
+    cost_distribution = options.tech_cost_distribution
+    min_cost = options.min_tech_cost.value
+    max_cost = options.max_tech_cost.value
+
+    match cost_distribution:
+        case cost_distribution.option_even:
+            science_location_costs = (random.randint(min_cost, max_cost) for _ in science_locations)
+        case cost_distribution.option_low:
+            science_location_costs = (random.triangular(min_cost, max_cost, min_cost) for _ in science_locations)
+        case cost_distribution.option_middle:
+            science_location_costs = (random.triangular(min_cost, max_cost, (min_cost + max_cost) // 2) for _ in science_locations)
+        case cost_distribution.option_high:
+            science_location_costs = (random.triangular(min_cost, max_cost, max_cost) for _ in science_locations)
+
+    science_location_costs = sorted(science_location_costs)
+
+    if options.ramping_tech_costs:
+        sorter = lambda location: location.complexity, location.cost
+    else:
+        sorter = lambda location: location.cost
+
+    for i, location in enumerate(sorted(science_locations, key=sorter)):
+        location.count = science_location_costs[i]
+
+    return science_locations
