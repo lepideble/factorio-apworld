8495837ba88ec6e2caa37b1db3c0e6e2ea56a8e7
lepideble <147614625+lepideble@users.noreply.github.com>
2026-04-06 20:57:41 +0200

    Add client

--- world/__init__.py
+++ world/__init__.py
@@ -1,1 +1,12 @@
+from worlds.LauncherComponents import Component, components, Type, launch as launch_component
+
 from .world import FactorioWorld
+from .config import game_name
+
+
+def launch_client(*args: str):
+    from .client import launch
+    launch_component(launch, name=f'{game_name} Client', args=args)
+
+
+components.append(Component(f'{game_name} Client', func=launch_client, component_type=Type.CLIENT))
