cfe80a1493a4609c53248e2d92d97b9997e59251
lepideble <147614625+lepideble@users.noreply.github.com>
2026-04-08 16:41:06 +0200

    WIP

--- world/world/rules.py
+++ world/world/rules.py
@@ -6,7 +6,7 @@ from ..config.rules import override_rules
 from ..data import craftable_recipes, machines_by_category, recipes, space_locations, surfaces
 
 from .locations import FactorioLocation, FactorioScienceLocation
-from .rules_classes import All, Any, CanAutomate, CanCraft, HasMachine, ReachedSpaceLocation, UnlockedRecipe, UnlockedSpaceLocation
+from .rules_classes import Any, AllRule, AnyRule, CanAutomate, CanCraft, HasMachine, ReachedSpaceLocation, UnlockedRecipe, UnlockedSpaceLocation
 
 
 def get_rules(locations: Iterable[FactorioLocation]) -> dict[str, Rule]:
@@ -20,20 +20,20 @@ def get_rules(locations: Iterable[FactorioLocation]) -> dict[str, Rule]:
 
             machines = [machine for machine in machines_by_category(recipe.category) if machine.can_be_placed_on(surface)]
 
-            has_machine_for_craft = Any([HasMachine(machine.name, surface.name) for machine in machines])
-            has_machine_for_automation = Any([CanCraft(machine.name, surface.name) for machine in machines])
+            has_machine_for_craft = AnyRule([HasMachine(machine.name, surface.name) for machine in machines])
+            has_machine_for_automation = AnyRule([CanCraft(machine.name, surface.name) for machine in machines])
 
             if recipe.category == 'asteroid-chunk':
                 if surface.is_space_platform:
                     space_locations_with_chunk = [space_location for space_location in space_locations if recipe.name in space_location.asteroid_chunks]
 
-                    reached_location = Any([ReachedSpaceLocation(space_location, surface) for space_location in space_locations_with_chunk])
+                    reached_location = ReachedSpaceLocation(Any(space_locations_with_chunk), surface)
 
                     rules[f'Craft {recipe.name} on {surface.name}'] = unlocked_recipe & has_machine_for_craft & reached_location
                     rules[f'Automate {recipe.name} on {surface.name}'] = unlocked_recipe & has_machine_for_automation & reached_location
             else:
-                can_craft_ingredients = All([CanCraft(ingredient_name, surface.name) for ingredient_name in recipe.ingredients.keys()])
-                can_automate_ingredients = All([CanAutomate(ingredient_name, surface.name) for ingredient_name in recipe.ingredients.keys()])
+                can_craft_ingredients = AllRule([CanCraft(ingredient_name, surface.name) for ingredient_name in recipe.ingredients.keys()])
+                can_automate_ingredients = AllRule([CanAutomate(ingredient_name, surface.name) for ingredient_name in recipe.ingredients.keys()])
 
                 rules[f'Craft {recipe.name} on {surface.name}'] = unlocked_recipe & has_machine_for_craft & can_craft_ingredients
                 rules[f'Automate {recipe.name} on {surface.name}'] = unlocked_recipe & has_machine_for_automation & can_automate_ingredients
@@ -43,12 +43,12 @@ def get_rules(locations: Iterable[FactorioLocation]) -> dict[str, Rule]:
                 if not space_location.accessible_at_start:
                     rules[f'Reach {space_location.name} with {surface.name}'] = (
                         UnlockedSpaceLocation(space_location.name)
-                            & Any([ReachedSpaceLocation(connection, surface) for connection in space_location.connections])
+                            & ReachedSpaceLocation(Any(space_location.connections), surface)
                     )
 
     for location in locations:
         if isinstance(location, FactorioScienceLocation):
-            rules[location.name] = All([Any([CanAutomate(science_pack, surface.name) for surface in surfaces]) for science_pack in location.ingredients.keys()])
+            rules[location.name] = CanAutomate(All(location.ingredients.keys()), Any(surfaces))
 
     override_rules(locations, rules)
 
--- world/world/rules_classes.py
+++ world/world/rules_classes.py
@@ -1,16 +1,79 @@
+from collections.abc import Callable
 from dataclasses import dataclass
+from typing import Generic, TypeVar
 
 from rule_builder.rules import And, Has, HasAny, Or, Rule, True_
 
 from ..config import game_name
 from ..data import machines_available_at_start, recipes_by_product, recipes_unlocked_at_start, space_locations, technologies, technologies_by_recipe_unlocked, technologies_by_space_location_unlocked
-from ..data_classes import SpaceLocation, Surface
+from ..data_classes import Base, SpaceLocation, Surface
 
 from .items import upgrades_map, upgrades_levels
 
 
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+@dataclass
+class All(Generic[T]):
+    children: list[T]
+
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__}({', '.join(self.children)})'
+
+
+@dataclass
+class Any(Generic[T]):
+    children: list[T]
+
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__}({', '.join(self.children)})'
+
+
+type Value[T] = T | All[T] | Any[T]
+
+
+def _value_map(function: Callable[[T], U], value: Value[T]) -> Value[U]:
+    if isinstance(value, All):
+        return All([function(child) for child in value.children])
+
+    if isinstance(value, Any):
+        return Any([function(child) for child in value.children])
+
+    return function(value)
+
+
+def _get_name(value: Base | str) -> str:
+    if isinstance(value, Base):
+        return value.name
+
+    return value
+
+
+def _value_expand(function, value, *values) -> Rule:
+    if len(values) > 0:
+        expander = lambda child: _value_expand(lambda *rest: function(child, *rest), *values)
+    else:
+        expander = function
+
+    if isinstance(value, All):
+        if len(value.children) == 0:
+            return True_()
+
+        return And(*[expander(child) for child in value.children])
+
+    if isinstance(value, Any):
+        if len(value.children) == 0:
+            return False_()
+
+        return Or(*[expander(child) for child in value.children])
+
+    return expander(value)
+
+
 @dataclass()
-class All(Rule['FactorioWorld'], game=game_name):
+class AllRule(Rule['FactorioWorld'], game=game_name):
     rules: list[Rule]
 
     def _instantiate(self, world: 'FactorioWorld') -> Rule.Resolved:
@@ -20,7 +83,7 @@ class All(Rule['FactorioWorld'], game=game_name):
         return And(*self.rules).resolve(world)
 
 @dataclass()
-class Any(Rule['FactorioWorld'], game=game_name):
+class AnyRule(Rule['FactorioWorld'], game=game_name):
     rules: list[Rule]
 
     def _instantiate(self, world: 'FactorioWorld') -> Rule.Resolved:
@@ -97,7 +160,7 @@ class UnlockedRecipe(Rule['FactorioWorld'], game=game_name):
         if len(technologies) == 0:
             raise Exception(f'No technology unlocks recipe "{self.name}"')
 
-        return Any([HasTechnology(technology.name) for technology in technologies]).resolve(world)
+        return AnyRule([HasTechnology(technology.name) for technology in technologies]).resolve(world)
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}({self.name})"
@@ -116,26 +179,29 @@ class UnlockedSpaceLocation(Rule['FactorioWorld'], game=game_name):
         if len(technologies) == 0:
             raise Exception(f'No technology unlocks space location "{self.name}"')
 
-        return Any([HasTechnology(technology.name) for technology in technologies]).resolve(world)
+        return AnyRule([HasTechnology(technology.name) for technology in technologies]).resolve(world)
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}({self.name})"
 
 
 class ReachedSpaceLocation(Rule['FactorioWorld'], game=game_name):
-    space_location_name: str
-    surface_name: str
+    space_location_name: Value[str]
+    surface_name: Value[str]
 
-    def __init__(self, space_location: SpaceLocation | str, surface: Surface | str):
+    def __init__(self, space_location: Value[SpaceLocation | str], surface: Value[Surface | str]):
         super().__init__()
-        self.space_location_name = space_location.name if isinstance(space_location, SpaceLocation) else space_location
-        self.surface_name = surface.name if isinstance(surface, Surface) else surface
+        self.space_location_name = _value_map(_get_name, space_location)
+        self.surface_name = _value_map(_get_name, surface)
 
     def _instantiate(self, world: 'FactorioWorld') -> Rule.Resolved:
-        if space_locations[self.space_location_name].accessible_at_start:
-            return True_().resolve(world)
+        def get_rule(space_location_name: str, surface_name: str) -> Rule:
+            if space_locations[space_location_name].accessible_at_start:
+                return True_()
 
-        return Has(f'Reach {self.space_location_name} with {self.surface_name}').resolve(world)
+            return Has(f'Reach {space_location_name} with {surface_name}')
+
+        return _value_expand(get_rule, self.space_location_name, self.surface_name).resolve(world)
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}(space_location={self.space_location_name}, surface={self.surface_name})"
