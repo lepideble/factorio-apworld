d630a3a1c0446939c21d84ae193008d48ca2ba18
lepideble <147614625+lepideble@users.noreply.github.com>
2026-07-05 15:52:38 +0200

    Register craft production for all recipes

--- base/world/productions/crafting.py
+++ base/world/productions/crafting.py
@@ -1,30 +1,35 @@
-from rule_builder.rules import Rule
+from rule_builder.rules import Rule, True_
 
 from ...data.classes import Surface
 from ...data.lookup import machines_by
 from ...data.raw import machines_for_manual_craft
-from ...data.utils import craftable_recipes
+from ...data.utils import unlockable_recipes
 from ..rules import All, Any, CanAutomate, CanCraft, UnlockedRecipe
 
 def get_crafting_productions(surface: Surface) -> dict[str, tuple[Rule, dict[str, bool]]]:
     events = {}
 
-    for recipe in craftable_recipes:
+    for recipe in unlockable_recipes:
         machines = machines_by(can_be_placed_on=surface, crafting_category=recipe.category)
 
         if len(machines) == 0:
             continue
 
-        if len(machines_for_manual_craft.intersection((machine.name for machine in machines))) > 0:
-            events[f'Craft {recipe.name}'] = (
-                UnlockedRecipe(recipe)
-                    & All([CanCraft(item_name, surface) for item_name in recipe.ingredients]),
-                {product_name: False for product_name in recipe.products},
-            )
+        can_manual_craft = len(machines_for_manual_craft.intersection((machine.name for machine in machines))) > 0
+
+        unlocked_recipe_rule = UnlockedRecipe(recipe)
+        has_machine_rule = Any([CanCraft(machine.name, surface) for machine in machines])
+
+        events[f'Craft {recipe.name}'] = (
+            unlocked_recipe_rule
+                & (True_() if can_manual_craft else has_machine_rule)
+                & All([CanCraft(item_name, surface) for item_name in recipe.ingredients]),
+            {product_name: False for product_name in recipe.products},
+        )
 
         events[f'Automate {recipe.name} crafting'] = (
-            UnlockedRecipe(recipe)
-                & Any([CanCraft(machine.name, surface) for machine in machines])
+            unlocked_recipe_rule
+                & has_machine_rule
                 & All([CanAutomate(item_name, surface) for item_name in recipe.ingredients]),
             {product_name: True for product_name in recipe.products},
         )
